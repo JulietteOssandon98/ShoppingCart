5ec59ef00336f6f9ed2848894a2b6273c18e1bbe
Juliette Ossandón <juliette.ossandon@inacapmail.cl>
2020-10-10 21:36:30 -0300

    SC-2 (feature): Add proct list in proncipal container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import { ProductList } from './components/products/ProductList';
 
 function App() {
   return (
-    <div className="root">
-      <div className="container"></div>
+    <div className="principal_container">
+      <div className="middle_container">
+        <ProductList />
+      </div>
     </div>
   );
 }
